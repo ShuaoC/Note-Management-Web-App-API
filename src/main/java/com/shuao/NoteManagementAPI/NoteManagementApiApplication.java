c946e981ad3b10d0d3f4bf1be3543db7311bdbab
ShuaoC <felix19981228@gmail.com>
2022-04-08 13:16:48 -0400

    First commit

---
+++ src/main/java/com/shuao/NoteManagementAPI/NoteManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.shuao.NoteManagementAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shuao/NoteManagementAPI/NoteManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuao.NoteManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
