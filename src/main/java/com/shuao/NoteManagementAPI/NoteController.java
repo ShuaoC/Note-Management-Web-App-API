0bb0819e83c1a24dda600766d3a9bd17859b982c
ShuaoC <felix19981228@gmail.com>
2022-04-19 16:34:33 -0400

    Saving changes

--- src/main/java/com/shuao/NoteManagementAPI/NoteController.java
+++ src/main/java/com/shuao/NoteManagementAPI/NoteController.java
@@ -1,19 +1,27 @@
 package com.shuao.NoteManagementAPI;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/bk")
 public class NoteController {
 
+    @Autowired
+    private userRepo userRepo;
+
     @PostMapping("/users")
-    public User createUser(){
-        User user = new User(UUID.randomUUID().toString(),"" + System.currentTimeMillis());
-        return user;
+    public User createUser(User user){
+        user = new User(UUID.randomUUID().toString(),"" + System.currentTimeMillis());
+        return userRepo.save(user);
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String testing(){
+        return "Testing";
     }
 
 }
