89cd8e35259487d1461702aabd733211b7bef2b2
ShuaoC <felix19981228@gmail.com>
2022-04-19 15:08:10 -0400

    Added dynamoDB dependency as well as created user class

---
+++ src/main/java/com/shuao/NoteManagementAPI/User.java
@@ -0,0 +1,12 @@
+package com.shuao.NoteManagementAPI;
+
+public class User {
+    private String id;
+    private String dateCreated;
+
+    public User(String id, String dateCreated) {
+        this.id = id;
+        this.dateCreated = dateCreated;
+    }
+
+}
