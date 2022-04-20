5a9729967945dd9c59d06ed13abb8a5e30129c6f
ShuaoC <felix19981228@gmail.com>
2022-04-20 17:42:04 -0400

    Saving changes

--- src/main/java/com/shuao/NoteManagementAPI/NoteController.java
+++ src/main/java/com/shuao/NoteManagementAPI/NoteController.java
@@ -1,7 +1,9 @@
 package com.shuao.NoteManagementAPI;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 
 import java.util.UUID;
 
@@ -10,8 +12,21 @@ import java.util.UUID;
 @RequestMapping("/bk")
 public class NoteController {
 
+//    private final DynamoDbTable<User> table;
+//    NoteController(DynamoDbTable<User> table) {
+//        this.table = table;
+//    }
+
+
     @Autowired
     private userRepo userRepo;
+
+//    @PostMapping("/users")
+//    public String createUser(){
+//        User user = new User(UUID.randomUUID().toString(),"" + System.currentTimeMillis());
+//        this.table.putItem(user);
+//        return "Success";
+//    }
 
     @PostMapping("/users")
     public User createUser(User user){
--- src/main/java/com/shuao/NoteManagementAPI/NoteManagementApiApplication.java
+++ src/main/java/com/shuao/NoteManagementAPI/NoteManagementApiApplication.java
@@ -2,6 +2,12 @@ package com.shuao.NoteManagementAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @SpringBootApplication
 public class NoteManagementApiApplication {
@@ -10,4 +16,20 @@ public class NoteManagementApiApplication {
 		SpringApplication.run(NoteManagementApiApplication.class, args);
 	}
 
+//	@Bean
+//	public DynamoDbTable<User> buildTable() {
+//		DynamoDbClient client = DynamoDbClient.builder()
+//				.region(Region.US_EAST_1)
+//				.build();
+//
+//		DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
+//				.dynamoDbClient(client)
+//				.build();
+//
+//		DynamoDbTable<User> table =
+//				enhancedClient.table("User_ID_password", TableSchema.fromBean(User.class));
+//
+//		return table;
+//
+//	}
 }
