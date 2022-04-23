539a39f635eb368134c92f752090d33ff9dda765
ShuaoC <felix19981228@gmail.com>
2022-04-23 18:30:23 -0400

    Added zxing dependency

--- src/main/java/com/shuao/NoteManagementAPI/NoteController.java
+++ src/main/java/com/shuao/NoteManagementAPI/NoteController.java
@@ -4,13 +4,19 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 import com.amazonaws.services.dynamodbv2.model.PutItemRequest;
 import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.model.PageIterable;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -23,7 +29,7 @@ public class NoteController {
     private final DynamoDbClient dynamoClient;
     private AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
     private ScanRequest scanRequest = new ScanRequest()
-            .withTableName("Reply");
+            .withTableName("User_ID_password");
 
     NoteController(DynamoDbTable<User> table, DynamoDbClient dynamoClient) {
         this.userTable = table;
@@ -38,9 +44,18 @@ public class NoteController {
     }
 
     @GetMapping("/users")
-    public String fetchUser(){
-        return "";
+    public List fetchUser(){
+        ArrayList<User> list = new ArrayList<User>();
+        ScanResult result = client.scan(scanRequest);
+
+//        ArrayList<User> list = new ArrayList<User>();
+//        PageIterable<User> result = userTable.scan();
+
+        return result.getItems();
     }
+
+    @GetMapping("/users/{id}/qr")
+
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
     public String testing(){
