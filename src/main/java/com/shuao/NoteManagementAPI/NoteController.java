d3c08de022803e61ea5e8791b9bbcf9a15676f02
ShuaoC <felix19981228@gmail.com>
2022-04-25 20:18:23 -0400

    QR code generation api implemented

--- src/main/java/com/shuao/NoteManagementAPI/NoteController.java
+++ src/main/java/com/shuao/NoteManagementAPI/NoteController.java
@@ -12,7 +12,11 @@ import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
 import software.amazon.awssdk.enhanced.dynamodb.model.DeleteItemEnhancedRequest;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
 import java.util.function.Consumer;
 
@@ -28,6 +32,7 @@ public class NoteController {
             .withTableName("User_ID_password");
     private ScanRequest NoteScanRequest = new ScanRequest()
             .withTableName("notes");
+    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/QRCode.png";
 
     NoteController(DynamoDbTable<User> table, DynamoDbTable<Note> noteTable, DynamoDbClient dynamoClient) {
         this.userTable = table;
@@ -53,7 +58,15 @@ public class NoteController {
         return result.getItems();
     }
 
-    //@GetMapping("/users/{id}/qr")
+    @GetMapping("/users/{id}/qr")
+    public void generateQR(@PathVariable("id") String id) throws Exception {
+        QRcodeGenerator.generateQRCodeImage(id, 350, 350, QR_CODE_IMAGE_PATH);
+    }
+
+//    @GetMapping("/users/{id}/qr")
+//    public ResponseEntity<byte[]> generateQRCode(@PathVariable("id") String id) throws Exception {
+//        return ResponseEntity.status(HttpStatus.OK).body(QRcodeGenerator.getQRCodeImage(id, 350, 350));
+//    }
 
     @PostMapping("/users/{id}/notes")
     public Note createNote(@PathVariable String id){
